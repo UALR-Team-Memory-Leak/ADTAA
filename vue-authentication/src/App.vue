72038f2cbf024f4e362ada365c85aea7af360ee9
Davis Kohler <cdkohler@ualr.edu>
2022-05-05 20:24:37 -0500

    working Vue login and registration

---
+++ vue-authentication/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+<Navigation />
+<div class="contanter">
+  <div class="row">
+    <div class="col-md-6 offset-md-3">
+      <div>
+        <img alt="UALR logo" src="./assets/ualr.png">
+        <router-view></router-view>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+</template>
+
+<script>
+import Navigation from './components/Navigation.vue'
+export default {
+  name: 'App',
+  components: {
+    Navigation,
+  }
+}
+</script>
---
+++ vue-authentication/src/pages/Register.vue
@@ -0,0 +1,37 @@
+<template>
+<div class="row">
+    <div class="col-md-6 offset-md-3">
+        <div>
+        <div>
+            <h3>Register</h3>
+            <hr />
+        </div>
+       <form>
+           <div class="form-group">
+               <label>Email</label>
+               <input type="text" class="form-control"/>
+           </div>
+           <div class="form-group">
+               <label>Username</label>
+               <input type="text" class="form-control"/>
+           </div>
+           <div class="form-group">
+               <label>Password</label>
+               <input type="password" class="form-control"/>
+           </div>
+
+           <div class="my-3">
+               <button type="submit" class="btn btn-primary">Register</button>
+           </div>
+       </form>
+    </div>
+    </div>
+</div>
+</template>
+<script>
+export default {
+  name: 'Register-item',
+  components: {
+  }
+}
+</script>
