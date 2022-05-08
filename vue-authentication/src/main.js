41a56d6f6486d906a6ae35e1aea3f8f5db0d6e73
Davis Kohler <cdkohler@ualr.edu>
2022-05-08 14:38:35 -0500

    maybe ready for deployment

--- vue-authentication/src/main.js
+++ vue-authentication/src/main.js
@@ -1,9 +1,14 @@
+//import { sync } from 'vuex-router-sync';
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import LoginValidations from './services/LoginValidations';
+import store from './store/store';
+
 
 const app= createApp(App);
+//sync(store. router);
 app.use(router);
+app.use(store);
 app.use(new LoginValidations);
 app.mount('#app');
