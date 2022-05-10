361abfba592079445330124f537dc42365eae476
JustinDady7 <56369832+JustinDady7@users.noreply.github.com>
2022-05-10 14:57:01 -0500

    Got the schedule to print

--- client/src/pages/MySchedule.vue
+++ client/src/pages/MySchedule.vue
@@ -1,81 +1,23 @@
 <template>
 
-<div class='schedule'></div>
-
 <div class="row">
     <div class="col-md-6 offset-md-3">
            <div>
             <img alt="UALRlogo" src="../assets/ualr.png">
-        
-       <form>
-           <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">Course Reference #</th>
-      <th scope="col">Course Section #</th>
-      <th scope="col">Class</th>
-      <th scope="col">Instructor</th>
-      <th scope="col">Time Period</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row"></th>
-      <td>{{}}</td>
-      <td>{{}}</td>
-      <td>{{}}</td>
-      <td>{{}}</td>
-    </tr>
-    <tr>
-      <th scope="row"></th>
-      <td>{{}}</td>
-      <td>{{}}</td>
-      <td>{{}}</td>
-      <td>{{}}</td>
-    </tr>
-    <tr>
-      <th scope="row"></th>
-      <td>{{}}</td>
-      <td>{{}}</td>
-      <td>{{}}</td>
-      <td>{{}}</td>
-    </tr>
-    <tr>
-      <th scope="row"></th>
-      <td>{{}}</td>
-      <td>{{}}</td>
-      <td>{{}}</td>
-      <td>{{}}</td>
-    </tr>
-    <tr>
-      <th scope="row"></th>
-      <td>{{}}</td>
-      <td>{{}}</td>
-      <td>{{}}</td>
-      <td>{{}}</td>
-    </tr>
-  </tbody>
-</table>
-           <div class="my-3">
-               <div class="my-3">
-               <button v-on:click="getSchedules" type="submit" class="btn btn-primary" buttonText="Edit">Edit</button>
-           </div>
-               <router-link class="nav-link" to="/homepage">
-               <button type="submit" class="btn btn-primary" buttonText="Submit"></button>
-           </router-link>
-           </div>
-       </form>
     </div>
     </div>
 </div>
+
+<div class='schedule'></div>
+<br>
 </template>
 
 <script>
 
-const scheduleDiv = document.querySelector('div.schedule')
-let tableHeaders = ["Section CRN", "Section Number, Section Course Title", "Instructor Last Name"]
-
 const createScheduleTable = ()  => {
+  const scheduleDiv = document.querySelector('div.schedule')
+  let tableHeaders = ["CRN", "Section Number", "Course Title", "Instructor Last Name"]
+  
   while (scheduleDiv.firstChild)
   {
     scheduleDiv.removeChild(scheduleDiv.firstChild);
@@ -129,7 +71,7 @@ const appendSections = (singleSection, singleSectionIndex) => {
 }
 
 const getSchedules = () => {
-  axios.get('http://localhost:3333/api/v0/assistant/scheduler')
+  fetch('http://localhost:3333/api/v0/assistant/scheduler')
   .then(res => res.json())
   .then(sections => {
     createScheduleTable()
@@ -142,23 +84,23 @@ const getSchedules = () => {
   })
 }
 
-import axios from "axios"
-
 export default {
-    data() {
-    return {
-        schedule: null
-    }
-    },
-    methods: {
-      onScheduling: function(event) {
-        axios.get(
-            'http://localhost:3333/api/v0/assistant/scheduler',
-        ).then((response) => {
-          this.schedule = response;
-          console.log(response);
-        });
-      },
+  data() {
+      return {
+      };
   },
-}
+  mounted: function() {
+      this.$nextTick(function () 
+      {
+        console.log("HI")
+        this.schedule()
+      })
+  },
+  methods: {
+      schedule() {
+          getSchedules()
+          }
+      },
+  }
+
 </script>
