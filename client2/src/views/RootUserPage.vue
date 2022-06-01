8f0e4746fb21733839176c7c8e63ae38120bef5f
wgriff6 <willgv557@gmail.com>
2022-06-01 17:35:21 -0500

    comment

--- client2/src/views/RootUserPage.vue
+++ client2/src/views/RootUserPage.vue
@@ -68,7 +68,7 @@ export default ({
     //     },
 
 
-    onApprove() {
+    onApprove() { //why does this work?
         var url = ''
     if (process.env.NODE_ENV === 'development') {
       url = 'http://localhost:3333'
